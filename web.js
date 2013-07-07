be7ba81dc53b70aaa13ff2c829fd9f4755760cd9
Pavan Kulkarni <bkpavan@gmail.com>
2013-07-07 01:28:26 +0000

    changed web.js to read from a file and added index.html

--- web.js
+++ web.js
@@ -3,10 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(fromFile('index.html'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var fs = require('fs');
+var fromFile = function(fileName) {
+  return fs.readFileSync(fileName);
+}
