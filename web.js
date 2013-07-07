d96d7fb68efd8a5440d17ea227f7e15d2d580a3f
Pavan Kulkarni <bkpavan@gmail.com>
2013-07-07 01:34:35 +0000

    fixed a typo

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.header('Content-Type', 'text/html);'
+  response.header('Content-Type', 'text/html');
   response.send(fromFile('index.html'));
 });
 
